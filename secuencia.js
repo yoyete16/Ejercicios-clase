294e0bdda536cdb56770bc171a58b2a340e0ae2a
yoyete16 <91971870+yoyete16@users.noreply.github.com>
2022-11-22 13:42:33 +0100

    secuencia a 100 circulos

--- secuencia.js
+++ secuencia.js
@@ -1,17 +1,24 @@
+var total = 100;
+var x = 30;
+var y =30;
+
 function setup() { 
-createCanvas(500,200);
+createCanvas(800,800);
 background(125);
 
 
 
 }
 function draw(){
-var x = 50;
-
-while(x<width-25){
-ellipse(x, height/2, 25,25);
-
-x = x + 50;
-
+while(total > 0){
+    if(total % 10 == 0){
+    x = 30;
+    y= y + 30;
+    }else{
+    x = x + 30;
+    }
+ellipse(x,y, 25,25);
+total = total - 1;
 }
+
 }
